41d157152f777a8ca837860db53d651c02b252aa
Alejandro1895 <alejandrovilla.2013@gmail.com>
2016-02-26 16:08:43 -0500

    Organizar Date

--- Android/Labs/Lab1UI/app/src/main/java/co/edu/udea/compumovil/gr01/lab1ui/MailActivity.java
+++ Android/Labs/Lab1UI/app/src/main/java/co/edu/udea/compumovil/gr01/lab1ui/MailActivity.java
@@ -2,12 +2,34 @@ package co.edu.udea.compumovil.gr01.lab1ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MailActivity extends AppCompatActivity {
+
+    private EditText mDateDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail);
+
+        mDateDisplay = (EditText) findViewById(R.id.dateDisplay);
+    }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.timeButton:
+                DialogFragment timePickerFragment = new TimePickerFragment();
+                timePickerFragment.show(getFragmentManager(), "timePicker");
+                break;
+
+            case R.id.dateButton:
+                DialogFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(getFragmentManager(), "datePicker");
+                break;
+        }
+
     }
 }
