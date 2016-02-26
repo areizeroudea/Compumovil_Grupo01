270eca40a3901d4fa133480bf0bfd9715231a5a0
Alejandro1895 <alejandrovilla.2013@gmail.com>
2016-02-26 15:30:13 -0500

    Laboratorio

---
+++ Android/Labs/Lab1UI/app/src/main/java/co/edu/udea/compumovil/gr01/lab1ui/MailActivity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gr01.lab1ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail);
+    }
+}
