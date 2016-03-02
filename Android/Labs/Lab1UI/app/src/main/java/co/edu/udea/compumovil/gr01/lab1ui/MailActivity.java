0dec4c4f799471b3767a82c3db8c83e868f1b40b
alejandro1895 <alejandrovilla.2013@gmail.com>
2016-03-01 19:30:25 -0500

    mmm

--- Android/Labs/Lab1UI/app/src/main/java/co/edu/udea/compumovil/gr01/lab1ui/MailActivity.java
+++ Android/Labs/Lab1UI/app/src/main/java/co/edu/udea/compumovil/gr01/lab1ui/MailActivity.java
@@ -6,9 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+
+import java.util.StringTokenizer;
 
 public class MailActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener
 {
@@ -16,14 +25,92 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
     private int mYear;
     private int mMonth;
     private int mDay;
+    private EditText editNombres;
+    private EditText editApellidos;
+    private Button btnMostrar;
+    private TextView txtInformacion;
+    private RadioButton radioHombre;
+    private RadioButton radioMujer;
+    private Spinner spinner;
+    private AutoCompleteTextView autoPais;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail);
+        establecerReferencias();
 
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.hobbies_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+// Get a reference to the AutoCompleteTextView in the layout
+        autoPais = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+// Get the string array
+        String[] countries = getResources().getStringArray(R.array.countries);
+// Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> adapter2 =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        autoPais.setAdapter(adapter2);
+        autoPais.setThreshold(1);
+
+    }
+
+    public void establecerReferencias(){
+        editNombres = (EditText) findViewById(R.id.editText);
+        editApellidos = (EditText) findViewById(R.id.editText2);
         mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
+        btnMostrar = (Button) findViewById(R.id.button);
+        txtInformacion = (TextView) findViewById(R.id.textView3);
+        radioHombre = (RadioButton) findViewById(R.id.radio_hombre);
+        radioMujer = (RadioButton) findViewById(R.id.radio_mujer);
+        spinner = (Spinner) findViewById(R.id.spinner);
+    }
+    private String sexo;
+    public void onRadioButtonClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.radio_hombre:
+                if (checked)
+                    sexo="Masculino";
+                    break;
+            case R.id.radio_mujer:
+                if (checked)
+                    sexo="Femenino";
+                    break;
+        }
+    }
+
+
+    String hobbie;
+    public void mostrarInformacion(View v){
+        String Nombres = editNombres.getText().toString();
+        String Apellidos = editApellidos.getText().toString();
+        String Fecha = mDateDisplay.getText().toString();
+        hobbie = spinner.getSelectedItem().toString();
+        String pais = autoPais.getText().toString();
+        String favorito;
+        if(((CheckBox) findViewById(R.id.checkBox)).isChecked()){
+            favorito = getString(R.string.txtFavoritoSi);
+        }else{
+            favorito=getString(R.string.txtFavoritoNo);
+        }
+        txtInformacion.setText("Nombres: "+Nombres + System.getProperty("line.separator")+
+                               "Apellidos: "+Apellidos+ System.getProperty("line.separator")+
+                               "Sexo: "+sexo+System.getProperty("line.separator")+
+                               "Fecha: "+Fecha+System.getProperty("line.separator")+
+                               "Hobbie: "+hobbie+System.getProperty("line.separator")+
+                               "Favorito: "+favorito+System.getProperty("line.separator")+
+                               "Pais: "+pais);
     }
 
     /*Identificamos cuando se hace click sobre algun elemento*/
@@ -42,22 +129,26 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
 
     //metodo implementado debido a OnDateSetListener para que el picker de fecha (DatePickerFragment) realice una accion cuando se setee la fecha
     @Override
-    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
-    {
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
         Log.v("MailActivity", "Setiando la fecha");
         mYear = year;
         mMonth = monthOfYear + 1;
-        String month = ""+mMonth;
-        if(mMonth < 10)
-        {
-            month = "0"+mMonth;
+        String month = "" + mMonth;
+        if (mMonth < 10) {
+            month = "0" + mMonth;
         }
         mDay = dayOfMonth;
-        String day = ""+(mDay);
-        if(mDay <10)
-        {
+        String day = "" + (mDay);
+        if (mDay < 10) {
             day = "0" + (mDay);
         }
-        mDateDisplay.setText(""+mYear+"-"+month+"-"+day);
+        mDateDisplay.setText("" + mYear + "-" + month + "-" + day);
     }
+
+
+
+
+
+
+
 }
