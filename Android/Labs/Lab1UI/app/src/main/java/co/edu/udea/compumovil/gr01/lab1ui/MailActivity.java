0e07cc4f01321de8b8024c5f5ef9e0a1b8062988
aareiza <aareiza@tredasolutions.com>
2016-03-02 20:53:31 -0500

    Configurado landscape y valñidados los datos

--- Android/Labs/Lab1UI/app/src/main/java/co/edu/udea/compumovil/gr01/lab1ui/MailActivity.java
+++ Android/Labs/Lab1UI/app/src/main/java/co/edu/udea/compumovil/gr01/lab1ui/MailActivity.java
@@ -1,18 +1,16 @@
 package co.edu.udea.compumovil.gr01.lab1ui;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
-=======
-import android.widget.Button;
->>>>>>> 31a1cab604d58f252ef18384750f2ba6668dc196
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -21,6 +19,9 @@ import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.StringTokenizer;
 
 public class MailActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener
@@ -31,19 +32,18 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
     private int mDay;
     private EditText editNombres;
     private EditText editApellidos;
+    private EditText editTelefono;
+    private EditText editDireccion;
+    private EditText editEmail;
     private Button btnMostrar;
     private TextView txtInformacion;
     private RadioButton radioHombre;
     private RadioButton radioMujer;
-<<<<<<< HEAD
     private Spinner spinner;
     private AutoCompleteTextView autoPais;
-=======
-    private Spinner spinHobbies;
->>>>>>> 31a1cab604d58f252ef18384750f2ba6668dc196
-
-
-
+    private String sexo;
+    String hobbie;
+    final Context context = this;   //necesario para alerts
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -51,7 +51,7 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail);
         establecerReferencias();
-<<<<<<< HEAD
+        sexo = "Masculino";
 
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -70,14 +70,10 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
         autoPais.setAdapter(adapter2);
         autoPais.setThreshold(1);
-
-=======
-        spinHobbies.setOnItemClickListener(R.id.spinner);
-
->>>>>>> 31a1cab604d58f252ef18384750f2ba6668dc196
     }
 
-    public void establecerReferencias(){
+    public void establecerReferencias()
+    {
         editNombres = (EditText) findViewById(R.id.editText);
         editApellidos = (EditText) findViewById(R.id.editText2);
         mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
@@ -85,17 +81,17 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
         txtInformacion = (TextView) findViewById(R.id.textView3);
         radioHombre = (RadioButton) findViewById(R.id.radio_hombre);
         radioMujer = (RadioButton) findViewById(R.id.radio_mujer);
-<<<<<<< HEAD
-        spinner = (Spinner) findViewById(R.id.spinner);
-=======
-        spinHobbies = (Spinner) findViewById(R.id.spinner);
->>>>>>> 31a1cab604d58f252ef18384750f2ba6668dc196
+        spinner = (Spinner) findViewById(R.id.sec1HobbiesSpin);
+        editTelefono = (EditText) findViewById(R.id.sec1Telefono);
+        editDireccion = (EditText) findViewById(R.id.sec1Direccion);
+        editEmail = (EditText) findViewById(R.id.sec1Email);
     }
-    private String sexo;
-    public void onRadioButtonClicked(View view) {
 
+    public void onRadioButtonClicked(View view)
+    {
         boolean checked = ((RadioButton) view).isChecked();
-        switch(view.getId()) {
+        switch(view.getId())
+        {
             case R.id.radio_hombre:
                 if (checked)
                     sexo="Masculino";
@@ -107,37 +103,59 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
         }
     }
 
-<<<<<<< HEAD
-
-    String hobbie;
-=======
->>>>>>> 31a1cab604d58f252ef18384750f2ba6668dc196
-    public void mostrarInformacion(View v){
+    public void mostrarInformacion(View v)
+    {
         String Nombres = editNombres.getText().toString();
         String Apellidos = editApellidos.getText().toString();
         String Fecha = mDateDisplay.getText().toString();
-<<<<<<< HEAD
-        hobbie = spinner.getSelectedItem().toString();
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-mm-dd");
+        try
+        {
+            Date fechaDate = formato.parse(Fecha);
+        }
+        catch (ParseException e)
+        {
+            Fecha = "";
+        }
+
         String pais = autoPais.getText().toString();
+        String telefono = editTelefono.getText().toString();
+        String direccion = editDireccion.getText().toString();
+        String email = editEmail.getText().toString();
+        hobbie = spinner.getSelectedItem().toString();
+
         String favorito;
-        if(((CheckBox) findViewById(R.id.checkBox)).isChecked()){
+        if(((CheckBox) findViewById(R.id.checkBox)).isChecked())
+        {
             favorito = getString(R.string.txtFavoritoSi);
-        }else{
+        }
+        else
+        {
             favorito=getString(R.string.txtFavoritoNo);
         }
+
+        //Antes que anda validamos que los datpos esten completos
+        if(Nombres.equals("") || Apellidos.equals("") || sexo.equals("") || Fecha.equals("") || pais.equals("")
+                || telefono.equals("") || direccion.equals("") || email.equals("") || hobbie.equals("") || favorito.equals(""))
+        {
+            AlertDialog.Builder alertBuild = new AlertDialog.Builder(context);
+            alertBuild.setTitle("Informacion incompleta");
+            alertBuild.setMessage("Por favor completa toda la informacion requerida");
+            AlertDialog alertDialog = alertBuild.create();
+            alertDialog.show();
+            return;
+        }
+
         txtInformacion.setText("Nombres: "+Nombres + System.getProperty("line.separator")+
                                "Apellidos: "+Apellidos+ System.getProperty("line.separator")+
                                "Sexo: "+sexo+System.getProperty("line.separator")+
-                               "Fecha: "+Fecha+System.getProperty("line.separator")+
+                               "Fecha nacimiento: "+Fecha+System.getProperty("line.separator")+
+                                "Pais: "+pais+System.getProperty("line.separator")+
+                                "Telefono: "+telefono+System.getProperty("line.separator")+
+                                "Direccion: "+direccion+System.getProperty("line.separator")+
+                                "Email: "+email+System.getProperty("line.separator")+
                                "Hobbie: "+hobbie+System.getProperty("line.separator")+
-                               "Favorito: "+favorito+System.getProperty("line.separator")+
-                               "Pais: "+pais);
-=======
-        txtInformacion.setText("Nombres: "+Nombres + System.getProperty("line.separator")+
-                               "Apellidos: "+Apellidos+ System.getProperty("line.separator")+
-                               "Sexo: "+sexo+System.getProperty("line.separator")+
-                               "Fecha: "+Fecha);
->>>>>>> 31a1cab604d58f252ef18384750f2ba6668dc196
+                               "Favorito: "+favorito+System.getProperty("line.separator"));
     }
 
     /*Identificamos cuando se hace click sobre algun elemento*/
@@ -171,11 +189,4 @@ public class MailActivity extends AppCompatActivity implements DatePickerDialog.
         }
         mDateDisplay.setText("" + mYear + "-" + month + "-" + day);
     }
-
-
-
-
-
-
-
 }
